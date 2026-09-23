9d6fccc0266eeb46c12fc75155094b48f3e8f217
JBoyle-C <jeffreyboyleit@gmail.com>
2026-09-22 17:43:22 -0700

    update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -1,10 +1,17 @@
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.vpc_id
-    map_public_ip_on_launch = true
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.vpc_id
+  map_public_ip_on_launch = true
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = true
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = "us-east-1b"
 }
