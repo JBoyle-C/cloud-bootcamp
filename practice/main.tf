c77045a8adb2b1a6e324059e95cb14df5ae86fca
JBoyle-C <jeffreyboyleit@gmail.com>
2026-09-25 18:14:57 -0700

    add vpc resource

--- practice/main.tf
+++ practice/main.tf
@@ -1,18 +1,7 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = "192.168.0.0/16"
-}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.vpc_id
-  map_public_ip_on_launch = true
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "practice-vpc"
+  }
 }
-
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = true
-  cidr_block              = "10.0.11.0/24"
-  availability_zone       = "us-east-1b"
-}
-learning terraform
