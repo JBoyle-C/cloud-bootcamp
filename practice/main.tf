6a2022827ba3a287b3c5dd1d1287d0daf76c049d
JBoyle-C <jeffreyboyleit@gmail.com>
2026-09-25 19:11:13 -0700

    added internet gateway

--- practice/main.tf
+++ practice/main.tf
@@ -78,3 +78,12 @@ resource "aws_subnet" "data_b" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "practice-igw"
+    Environment = "dev"
+  }
+}
+
